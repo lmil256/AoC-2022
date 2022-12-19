a64ec9cd3899ae68e1df61abec30f3bc1edc0f8e
Laurence Miller <52897574+lmil256@users.noreply.github.com>
2022-12-18 21:47:04 -0500

    Day 6 part 1

---
+++ day6/part1.py
@@ -0,0 +1,14 @@
+TARGET = 4
+with open('input.txt') as infile:
+    seq = ''
+    numread = 0
+    while len(seq) < TARGET:
+        # Read a character
+        seq += infile.read(1)
+        numread += 1
+        # Search for the last character earlier in the sequence
+        if (p := seq.find(seq[-1], 0, -1)) != -1:
+            # If found, truncate the sequence past the match
+            seq = seq[p+1:]
+
+print(numread)
